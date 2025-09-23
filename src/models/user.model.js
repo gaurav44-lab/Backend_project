03a47b8195cc2a40427b3635b3906fb52f2127e4
gaurav44-lab <gauravyadav412004@gmail.com>
2025-09-23 20:56:33 +0530

    video and user model

---
+++ src/models/user.model.js
@@ -0,0 +1,97 @@
+import mongoose , {schema} from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const user_detail = new mongoose.Schema({
+username : {
+    type : true ,
+    required : true ,
+    unique :  true ,
+    lowercase : true ,
+    trim :  true ,
+    index : true
+
+} ,
+
+email : {
+    type : String ,
+    required : true ,
+    trim : true ,
+    index : true 
+} ,
+
+fullname : {
+    type : String ,
+    required : true ,
+    trim : true ,
+    index : true 
+} ,
+
+
+avatar : {
+    type : String ,
+    required : true ,
+    
+} ,
+
+coverimage : {
+    type : String ,
+    
+},
+
+coverimage : {
+    type : String ,
+    
+},
+
+watchHistory : [{
+    type : Schema.Types.ObjectId ,
+     ref : " video"
+    
+}] ,
+
+
+password : {
+    type : String ,
+    required : [true , 'password is mandatory'] ,
+
+    
+} ,
+refreshtoken : {
+    type : String ,
+     
+}
+
+},
+{timestamps : true})
+
+// checks the password to modify it every time
+
+user_detail.pre("save" , async function (next){
+     if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
+//  checks the password if it meets the criteria
+user_detail.methods.isPasswordCorrect = async function (password){
+   return await bcrypt.compare(password , this.password)
+}
+user_detail.methods.generateAccessToken = function(){
+    return jwt.sign(
+
+        {
+            _id : this._id ,
+            email : this.email ,
+            username : this.username ,
+            fullname : this.fullname 
+
+        } ,
+        process.env.Access_token_secret ,
+        {
+            expiresIn : process.env.access_token_expiry
+        }
+    )
+}
+
+export const user = mongoose.model("user" , user_detail)
